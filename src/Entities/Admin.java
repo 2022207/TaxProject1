79060024529867ab16b3cbdca1394541cdaa21e8
2022345 <2022345@student.cct.ie>
2023-12-20 15:26:58 -0300

    Update Admin.java
    Created attributes and methods to Admin Class

--- src/Entities/Admin.java
+++ src/Entities/Admin.java
@@ -4,10 +4,61 @@
  */
 package Entities;
 
+import Database.DatabaseWriter;
+import java.util.ArrayList;
+
 /**
  *
  * @author suele
  */
-public class Admin {
+public class Admin extends AllUser {
+    
+      public Admin(int id, String name, String surname, String username, String email, String password, double grossIncome) {
+        super(id, name, surname, username, email, password, grossIncome);
+        
+    }
+
+    public void modifyProfile() {
+        // Implementation for modifying the profile
+        System.out.println("Profile modified for admin: " + getUsername());
+    }
+
+    
+    
+    public void accessUserList(ArrayList<AllUser> userList) {
+    // Implementation for accessing the list of all users
+    System.out.println("List of all users:");
+    for (AllUser user : userList) {
+        System.out.println("ID: " + user.getId() +
+                           ", Name: " + user.getName() +
+                           ", Surname: " + user.getSurname() +
+                           ", Username: " + user.getUsername() +
+                           ", Email: " + user.getEmail() +
+                           ", Gross Income: " + user.getgrossIncome());
+    }
+}
+
+
+    public void removeUser(ArrayList<AllUser> userList, String username) {
+    // Remove the user from the local list
+    userList.removeIf(user -> user.getUsername().equals(username));
+    System.out.println("User removed: " + username);
+
+    // Update the database to reflect the user removal
+    DatabaseWriter dbWriter = new DatabaseWriter();
+    if (dbWriter.removeUser(username)) {
+        System.out.println("User removed from the database: " + username);
+    } else {
+        System.out.println("Failed to remove user from the database: " + username);
+    }
+}
+    
+   
+
+    public void reviewOperations() {
+        // Implementation for reviewing operations
+        System.out.println("Reviewing operations for admin: " + getUsername());
+    }
+    
     
 }
