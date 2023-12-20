9dcdeb061172e39bd7b453b4623c1ca2b74f1164
2022207 <2022207@student.cct.ie>
2023-12-20 18:32:51 +0000

    Set up database
    Database set up and connection made

--- src/Database/DatabaseSetup.java
+++ src/Database/DatabaseSetup.java
@@ -4,10 +4,61 @@
  */
 package Database;
 
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+
 /**
  *
  * @author castr
  */
-public class DatabaseSetup {
+public class DatabaseSetup  extends Database {
+   
+     
+
+    // Method to set up the database and create table
+    public static boolean setupDB() throws SQLException, ClassNotFoundException, InstantiationException, IllegalAccessException {
+        
+         // Load the MySQL JDBC driver
+        Class.forName("com.mysql.cj.jdbc.Driver").newInstance();
+        
+             try (
+                 //connection to the database
+                 Connection conn = DriverManager.getConnection(DB_BASE_URL, USER, PASSWORD);
+                     
+              //statement for executing SQL queries
+                Statement stmt = conn.createStatement() ;
+            ){
+                 
+        // Execute SQL queries to create the database and table
+            stmt.execute("CREATE DATABASE IF NOT EXISTS " + DB_NAME + ";");
+            stmt.execute("USE " + DB_NAME + ";");
+            String sql = 
+                    "CREATE TABLE IF NOT EXISTS " + TABLE_NAME + "  ("
+                    + "id INT(10)PRIMARY KEY AUTO_INCREMENT,"
+                    + "name VARCHAR(255),"
+                    + "surname VARCHAR(255),"
+                    + "username VARCHAR(255),"
+                    + "email VARCHAR(255),"
+                    + "password VARCHAR(255),"
+                    + "grossIncome DOUBLE"
+                    + ");";
+            stmt.execute(sql);
+            
+            // Return true to indicate successful database setup
+            return true;
+        
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+
+
+
     
-}
+    }
+
--- src/Database/DatabaseWriter.java
+++ src/Database/DatabaseWriter.java
@@ -4,10 +4,54 @@
  */
 package Database;
 
+import Entities.AllUser;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+//import java.util.List;
+
 /**
  *
  * @author castr
  */
-public class DatabaseWriter {
+public class DatabaseWriter extends Database {
+
+    
+    
+    public boolean addAllUser(AllUser allUser) throws SQLException {
+        try (
+                Connection conn = DriverManager.getConnection(DB_URL, USER, PASSWORD);
+                Statement stmt = conn.createStatement();
+        ) {
+            String sql = String.format("INSERT INTO %s (name, surname, username, email, password, grossIncome) VALUES ('%s', '%s', '%s', '%s', '%s', %.2f);",
+                     TABLE_NAME,allUser.getName(), allUser.getSurname(), allUser.getUsername(),  allUser.getEmail(), allUser.getPassword(), allUser.getgrossIncome());
+           
+            stmt.execute(sql);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+        
+   
+
+    public boolean removeUser(String username) {
+        try (Connection conn = DriverManager.getConnection(DB_BASE_URL, USER, PASSWORD);
+             Statement stmt = conn.createStatement()) {
+            
+             stmt.execute("USE " + DB_NAME + ";");
+            String sql = "DELETE FROM " + TABLE_NAME + " WHERE username = '" + username + "';";
+            int rowsAffected = stmt.executeUpdate(sql);
+            return rowsAffected > 0;
+        } catch (Exception e) {
+            e.printStackTrace();
+            return false;
+        }
+    }
+        
+        
+        
     
 }
