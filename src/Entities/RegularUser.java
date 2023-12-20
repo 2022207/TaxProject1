598d46d00e8b1b9e45ac0348db7a4194f8776203
2022345 <2022345@student.cct.ie>
2023-12-20 15:28:04 -0300

    Update RegularUser.java
    Created attributes and methods to RegularUser Class

--- src/Entities/RegularUser.java
+++ src/Entities/RegularUser.java
@@ -8,6 +8,20 @@ package Entities;
  *
  * @author suele
  */
-public class RegularUser {
+public class RegularUser extends AllUser {
+    
+     public RegularUser(int id, String name, String surname, String username, String email, String password, double grossIncome) {
+        super(id, name, surname, username, email, password, grossIncome);
+    }
+
+    public void modifyProfile() {
+        // Implementation for modifying the profile
+        System.out.println("Profile modified for regular user: " + getUsername());
+    }
+
+    public void saveAndSeeEquations() {
+        // Implementation for saving and seeing system of equations
+        System.out.println("Equations saved and seen for regular user: " + getUsername());
+    }
     
 }
